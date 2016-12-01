3b9d1c8d4fda23c00f25470e79d5f95b87de8d0f
Jental <jentalmeister@gmail.com>
2016-12-02 00:15:22 +0300

    Matrix processor stub

---
+++ course_1_week3/smoothing/src/com/smoothing/MatrixProcessor.java
@@ -0,0 +1,44 @@
+package com.smoothing;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Created by jental on 02/12/16.
+ */
+public class MatrixProcessor {
+
+    // Sums two matrices
+    public static int[][] sum(int[][] m1, int[][] m2) {
+        throw new NotImplementedException();
+    }
+
+    // Sums two vectors
+    public static int[] sum(int[] v1, int[] v2) {
+        throw new NotImplementedException();
+    }
+
+    // Multiplies two matrices
+    public static int[][] multiply(int[][] m1, int[][] m2) {
+        throw new NotImplementedException();
+    }
+
+    // Multiplies matrix and vector
+    public static int[] multiply(int[][] m1, int[] v) {
+        throw new NotImplementedException();
+    }
+
+    // Multiplies two vectors
+    public static int multiply(int[] v1, int[] v2) {
+        throw new NotImplementedException();
+    }
+
+    // Multiplies matrix and number
+    public static int multiply(int[][] m, int n) {
+        throw new NotImplementedException();
+    }
+
+    // Multiplies vector and number
+    public static int multiply(int[] v,int n) {
+        throw new NotImplementedException();
+    }
+}
